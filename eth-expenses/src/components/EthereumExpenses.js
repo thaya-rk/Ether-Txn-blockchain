05a85af20171b4a85093a7393c4bc5b5c4863d34
thayark <rkt12357@gmail.com>
2024-09-02 22:53:28 +0530

    fix:minor changes

--- eth-expenses/src/components/EthereumExpenses.js
+++ eth-expenses/src/components/EthereumExpenses.js
@@ -64,6 +64,7 @@ const Blob = styled('div')({
 // Helper function to validate Ethereum address
 const isValidEthereumAddress = (address) => {
   const regex = /^0x[a-fA-F0-9]{40}$/;
+  
   return regex.test(address);
 };
 
